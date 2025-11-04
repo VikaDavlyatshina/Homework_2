65ee9dbaacdc0e54a56fbe48de93ded8bc3cfa43
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2025-11-04 22:18:25 +0300

    Добавлено исключение, если пользователь создает товар с нулевым количеством

--- src/product.py
+++ src/product.py
@@ -37,6 +37,9 @@ class Product(ReprMixin, BaseProduct):
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
 
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен.")
+
         self.name = name
         self.description = description
         self.__price = price
